06e690c481673259c37654b825fdcf4867608616
Vadim Mokrushin <mokr.vo@yandex.ru>
2025-09-17 00:43:55 +0700

    feat: add slogans

--- src/pages/main/ui/MainPage.tsx
+++ src/pages/main/ui/MainPage.tsx
@@ -5,11 +5,17 @@ import styles from './MainPage.module.css';
 
 export const MainPage = () => {
   return (
-    <main>
+    <main className={styles.container}>
       <div
         style={getBackgroundImageStyle(IMAGE_URLS.mainPageBackground)}
         className={classNames(styles.background, ANIMATIONS.fadeIn)}
       />
+      <div className={styles.textIntroContainer}>
+        <span className={styles.slogan}>Making Humanity Multiplanetary</span>
+        <span className={styles.subtitle}>
+          Pioneering the future of space exploration and interplanetary life
+        </span>
+      </div>
     </main>
   );
 };
